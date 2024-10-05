c8a4e9e71d9f62ce0235b045608e2602f44c7ee9
Quanpham0110 <quanpham084@gmail.com>
2024-10-05 15:21:44 +0700

    bt_binary_tree_search

---
+++ bt.py
@@ -0,0 +1,30 @@
+# 13 20 4 2 14 22 5
+root={
+    'key':13,
+    'left':{
+        'key':4,
+        'left':{
+            'key':2,
+            'left':{},
+            'right':{}
+            },
+        'right':{
+            'key':5,
+            'left':{},
+            'right':{}
+        }
+        },
+    'right':{
+        'key':20,
+        'left':{
+            'key':14,
+            'left':{},
+            'right':{}
+            },
+        'right':{
+            'key':20,
+            'left':{},
+            'right':{}
+        }
+    }
+}
